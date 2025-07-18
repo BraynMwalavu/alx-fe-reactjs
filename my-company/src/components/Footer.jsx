fcf65dcd2429fc88d2f87c1d645f527ceb95e456
Brian Mwalavu <brianmwalavu@gmail.com>
2025-07-18 14:46:50 +0300

    Task 3, my-company

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', marginTop: '20px', padding: '10px', backgroundColor: '#474747ff' }}>
+      © 2025 My Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
