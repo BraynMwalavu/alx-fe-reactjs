b15349e489396e12255dc37bae4fddeb3d3ef82a
Brian Mwalavu <brianmwalavu@gmail.com>
2025-08-02 12:50:51 +0300

    Task 1: Advanced search on github API

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,10 +1,12 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_APP_GITHUB_API_URL;
+
 export const fetchUserData = async (query) => {
-  const response = await fetch(`https://api.github.com/search/users?q=${query}`);
-
-  if (!response.ok) {
-    throw new Error("Search failed");
+  try {
+    const response = await axios.get(`${BASE_URL}/search/users?q=${query}`);
+    return response.data.items; // this returns an array of users
+  } catch (error) {
+    throw error;
   }
-
-  const data = await response.json();
-  return data.items; // Returns array of users
 };
