0d42a9550af6ec500da877a2f93ce2cb4204c20d
Brian Mwalavu <brianmwalavu@gmail.com>
2025-08-23 14:01:49 +0300

    Added react-todo

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,30 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+test("renders initial todos", () => {
+  render(<TodoList />);
+  expect(screen.getByText("Learn React")).toBeInTheDocument();
+  expect(screen.getByText("Build a Todo App")).toBeInTheDocument();
+});
+
+test("can add a new todo", () => {
+  render(<TodoList />);
+  fireEvent.change(screen.getByPlaceholderText("Add a new todo"), {
+    target: { value: "New Task" },
+  });
+  fireEvent.click(screen.getByText("Add"));
+  expect(screen.getByText("New Task")).toBeInTheDocument();
+});
+
+test("can toggle a todo", () => {
+  render(<TodoList />);
+  const todo = screen.getByText("Learn React");
+  fireEvent.click(todo);
+  expect(todo).toHaveStyle("text-decoration: line-through");
+});
+
+test("can delete a todo", () => {
+  render(<TodoList />);
+  fireEvent.click(screen.getAllByText("Delete")[0]);
+  expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+});
