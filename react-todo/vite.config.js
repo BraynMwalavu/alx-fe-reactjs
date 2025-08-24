12e0c806c43931427159375a08b1691623b0a6af
Brian Mwalavu <brianmwalavu@gmail.com>
2025-08-24 17:17:11 +0300

    Editted TodoList.js

--- react-todo/vite.config.js
+++ react-todo/vite.config.js
@@ -1,14 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,           // allows using describe, test, expect directly
-    environment: 'jsdom',    // simulates browser for React
-    setupFiles: './src/setupTests.js',  // loads jest-dom matchers
-    transformMode: {
-      web: [/.[jt]sx?$/],    // ensures JSX is transformed in tests
-    },
-  },
-});
+})
